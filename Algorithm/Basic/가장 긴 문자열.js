860df6d2cb541906ff6f625a52aed14f863c592c
dongchan-K <ehdcks1028@gmail.com>
2021-05-04 00:27:56 +0900

    feat: 가장 긴 문자열 문제 풀이 추가

---
+++ Algorithm/Basic/가장 긴 문자열.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  let answer = '';
+  for (const x of s) {
+    if (x.length > answer.length) {
+      answer = x;
+    }
+  }
+  return answer;
+}
+let str = ['teacher', 'time', 'student', 'beautiful', 'good'];
+console.log(solution(str));
