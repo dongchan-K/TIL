d35d43433be85ed7fcd4b7269eabb6b3bdb2db80
dongchan-K <ehdcks1028@gmail.com>
2021-05-03 23:26:45 +0900

    feat: A를 #으로 문제풀이 추가

---
+++ Algorithm/Basic/A를 #으로.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  let answer = '';
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'A') {
+      answer += '#';
+    } else {
+      answer += s[i];
+    }
+  }
+
+  return answer;
+}
+
+const str = 'BANANA';
+console.log(solution(str));
